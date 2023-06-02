338de5137ca222b6a369f358e576eee60927159c
Jason Thomas <102771061+Jason-Thomas274@users.noreply.github.com>
2023-06-01 23:14:16 -0700

    Added strict equality checks

--- js/js.js
+++ js/js.js
@@ -10,21 +10,21 @@ function contactButton() {
     var errorCountL = 0;
     var errorCountE = 0;
 
-    if (fname == "" || !validChar.test(fname)) {
+    if (fname === "" || !validChar.test(fname)) {
         fnameError.style.display = "block";
         alert("Please enter your first name with no numbers or special characters");
         errorCountF = 1;
     } else {
         fnameError.style.display = "none";
     }
-    if (lname == "") {
+    if (lname === "") {
         lnameError.style.display = "block";
         errorCountL = 1;
         alert("Please enter your last name with no numbers or special characters");
     } else {
         lnameError.style.display = "none";
     }
-    if (emailadd == "" || !validateEmail(emailadd)) {
+    if (emailadd === "" || !validateEmail(emailadd)) {
         emailaddError.style.display = "block";
         alert("Please enter a valid email address");
         errorCountE = 1;
@@ -32,7 +32,7 @@ function contactButton() {
         emailaddError.style.display = "none";
     }
 
-    if (errorCountF == 1 || errorCountL == 1 || errorCountE == 1) {
+    if (errorCountF === 1 || errorCountL === 1 || errorCountE === 1) {
         return false;
     } else {
         alert("Thank you!");
